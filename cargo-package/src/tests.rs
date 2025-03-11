92b0ec18758d56526d2220eb44612db008cc8b0d
Neal Fachan <neal@fachan.com>
2025-03-11 09:35:43 -0700

    Add subprocess example.

--- cargo-package/src/tests.rs
+++ cargo-package/src/tests.rs
@@ -1,1 +1,2 @@
+mod subprocess;
 mod tempfile;
---
+++ cargo-package/src/tests/subprocess.rs
@@ -0,0 +1,21 @@
+use std::{process::Command, str};
+
+/// The use of [`Command`]'s `output` or `spawn` requires three things from the Maelstrom container:
+///   - If any of stdin, stdout, and stderr are [`std::process::Stdio::null()`], which is the
+///     default, then there needs to be a [`/dev/null`] file that always returns EOF. The default
+///     configuration contains this.
+///   - The program to be executed must exist in the container.
+///   - If the program to be executed has shared library dependencies, then the dependencies must
+///     also exist in the container.
+#[cfg(target_arch = "x86_64")]
+#[test]
+fn echo() {
+    let output = Command::new("echo")
+        .arg("Hello,")
+        .arg("World!")
+        .output()
+        .unwrap_or_else(|err| panic!("error executing subprocess: {err:?}"));
+    assert_eq!(str::from_utf8(&output.stderr).unwrap(), "");
+    assert!(output.status.success());
+    assert_eq!(str::from_utf8(&output.stdout).unwrap(), "Hello, World!\n");
+}
