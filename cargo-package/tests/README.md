6670223909e57d5c778bd11ac78ea340c84c9fe6
Neal Fachan <neal@fachan.com>
2025-03-11 14:59:58 -0700

    Add an example of an integration test executing a binary.

---
+++ cargo-package/tests/README.md
@@ -0,0 +1,5 @@
+These are the [integration
+tests](https://doc.rust-lang.org/book/ch11-03-test-organization.html#integration-tests).
+
+In pattern filters, these will be selected by the `test` [compound
+selector](https://maelstrom-software.com/doc/book/latest/cargo-maelstrom/filter.html#compound-selectors).
---
+++ cargo-package/tests/integration_test.rs
@@ -0,0 +1,19 @@
+use std::{env, process::Command, str};
+
+/// The use of [`Command`]'s `output` or `spawn` requires three things from the Maelstrom container:
+///   - If any of stdin, stdout, and stderr are [`std::process::Stdio::null()`], which is the
+///     default, then there needs to be a [`/dev/null`] file that always returns EOF. The default
+///     configuration contains this.
+///   - The program to be executed must exist in the container.
+///   - If the program to be executed has shared library dependencies, then the dependencies must
+///     also exist in the container.
+#[cfg(target_arch = "x86_64")]
+#[test]
+fn echo() {
+    let output = Command::new(env!("CARGO_BIN_EXE_say-hello"))
+        .output()
+        .unwrap_or_else(|err| panic!("error executing say-hello: {err:?}"));
+    assert_eq!(str::from_utf8(&output.stderr).unwrap(), "");
+    assert!(output.status.success());
+    assert_eq!(str::from_utf8(&output.stdout).unwrap(), "Hello, World!\n");
+}
